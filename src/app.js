eedc9b897f3887b666fbd7a739e6568a393111b8
Jess Montero <87496543+jessmontero@users.noreply.github.com>
2021-08-24 20:34:32 +0100

    Added insults, styling

--- src/app.js
+++ src/app.js
@@ -15,17 +15,32 @@
                 "'Thou have not so much brain as ear-wax.'",
                 "'You are not worth another word.'",
                 "'By my troth, I was seeking for a fool when I found you.'",
+                "'Thou damned and luxurious mountain goat.'",
+                "'Would thou wert clean enough to spit upon.'",
+                "'More of your conversation would infect my brain.'",
+                "'Away, you mouldy rogue, away!'",
+                "'I do wish thou were a dog, that I might love thee something.'",
+                "'A weasel hath not such a deal of spleen as you are toss'd with.'",
+                "'Me think'st thou art a general offence and every man should beat thee.'",
                 "'Out of my sight! Thou dost infect mine eyes.'",
                 "'Scratching could not make it worse... such a face as yours.'",
                 "'Thou smell of mountain goat.'",
                 "'Thou lump of foul deformity.'",
+                "'Away, you three-inch fool!'",
                 "'Thy wit’s as thick as Tewkesbury mustard.'",
                 "'I must tell you friendly in your ear, sell when you can, you are not for all markets.'",
+                "'I am sick when I do look on thee.'",
+                "'The tartness of his face sours ripe grapes.'",
+                "'Thine face is not worth sunburning.'",
+                "'Away thou rag, thou quantity, thou remnant.'",
+                "'What a thrice-double ass!'",
+                "'Thou art a boil, a plague sore.'",
+                "'Thou art unfit for any place but hell.'",
                 "'The rankest compound of villainous smell that ever offended nostril.'",
                 "'Your brain is as dry as the remainder biscuit after a voyage.'"
             ]
 
-            var randomNumber = Math.floor(Math.random() * (21));
+            var randomNumber = Math.floor(Math.random() * (36));
 
 
                 event.preventDefault();
@@ -35,5 +50,5 @@
 
             }
 
-            let insultButton = document.querySelector(".button");
+            let insultButton = document.querySelector("button");
             insultButton.addEventListener("click", insultUser);
